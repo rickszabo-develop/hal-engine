6b607796b8bf58e7c0bb6c85d0c0534395c1e411
Henrique Szabo <henrique.szabo@tappsvr.com>
2023-10-21 16:17:52 -0300

    feat: implement window and input events

--- HAL_Engine/HALENGINE/src/engine/Application.cpp
+++ HAL_Engine/HALENGINE/src/engine/Application.cpp
@@ -4,21 +4,35 @@
 #include "engine/events/ApplicationEvent.h"
 #include "engine/Log.h"
 
+#include <GLFW/glfw3.h>
+
 namespace Haleng 
 {
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	Application::~Application()
 	{
 	}
 
+	void Application::OnEvent(Event& e)
+	{
+		HALENG_CORE_INFO("{0}", e.ToString());
+	}
+
 	void Application::Run()
 	{
 		while (m_Running) 
 		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+
 			m_Window->OnUpdate();
 		}
 	}
--- HAL_Engine/HALENGINE/src/engine/events/Event.h
+++ HAL_Engine/HALENGINE/src/engine/events/Event.h
@@ -61,12 +61,12 @@ namespace Haleng {
 		EventDispatcher(Event& event)
 			:m_Event(event) {}
 
-		template<typename T>
-		bool Dispatch(EventFn<T> func)
+		template<typename T, typename F>
+		bool Dispatch(const F& func)
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.m_Handled = func(*(T*) &m_Event);
+				m_Event.m_Handled |= func(static_cast<T&>(m_Event));
 				return true;
 			}
 			return false;
