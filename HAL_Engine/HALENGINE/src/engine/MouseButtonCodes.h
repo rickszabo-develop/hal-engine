4c9dfba7cf32914e194e3fcb45684a5540c3e4e5
VTR085\Henrique Szabo <henrique.szabo@venturionvr.com>
2024-03-18 17:15:26 -0300

    chore: add haleng custom keycodes based on glfw3

---
+++ HAL_Engine/HALENGINE/src/engine/MouseButtonCodes.h
@@ -0,0 +1,22 @@
+#pragma once
+
+//From glfw3.h
+/*! @defgroup buttons Mouse buttons
+ *  @brief Mouse button IDs.
+ *
+ *  See [mouse button input](@ref input_mouse_button) for how these are used.
+ *
+ *  @ingroup input
+ *  @{ */
+#define HAL_MOUSE_BUTTON_1         0
+#define HAL_MOUSE_BUTTON_2         1
+#define HAL_MOUSE_BUTTON_3         2
+#define HAL_MOUSE_BUTTON_4         3
+#define HAL_MOUSE_BUTTON_5         4
+#define HAL_MOUSE_BUTTON_6         5
+#define HAL_MOUSE_BUTTON_7         6
+#define HAL_MOUSE_BUTTON_8         7
+#define HAL_MOUSE_BUTTON_LAST      HAL_MOUSE_BUTTON_8
+#define HAL_MOUSE_BUTTON_LEFT      HAL_MOUSE_BUTTON_1
+#define HAL_MOUSE_BUTTON_RIGHT     HAL_MOUSE_BUTTON_2
+#define HAL_MOUSE_BUTTON_MIDDLE    HAL_MOUSE_BUTTON_3
